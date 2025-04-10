7e5d423fed9264a61eb1697721739735384a4b0d
hjali7 <hjikingali@gmail.com>
2025-04-10 15:40:20 +0330

    add healchek and auth config for nginx

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "☁️ دریافت فایل از S3..."
-aws --endpoint-url=http://localstack:4566 s3 cp s3://local-bucket/index.html /usr/share/nginx/html/index.html
+aws --endpoint-url=http://localstack:4566 s3 cp s3://local-bucket/index.html  /usr/share/nginx/html/index.html
 
 echo "🚀 اجرای nginx"
 nginx -g 'daemon off;'
